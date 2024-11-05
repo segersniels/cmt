77b906004c7bb2441c94b886300f76c5c43e0296
segersniels <segers.n@hotmail.com>
2024-11-05 17:01:53 +0100

    refactor: simplify by just using an enum

--- config.go
+++ config.go
@@ -3,48 +3,14 @@ package main
 import (
 	"encoding/json"
 	"os"
-
-	log "github.com/charmbracelet/log"
 )
 
-type ConventionType int
+type ConventionType string
 
 const (
-	ConventionalCommitConvention ConventionType = iota
-	GitmojiConvention
+	ConventionalCommitConvention ConventionType = "conventional-commit"
+	GitmojiConvention            ConventionType = "gitmoji"
 )
-
-var conventionType = map[ConventionType]string{
-	ConventionalCommitConvention: "conventional-commit",
-	GitmojiConvention:            "gitmoji",
-}
-
-func (c ConventionType) String() string {
-	return conventionType[c]
-}
-
-func (c *ConventionType) UnmarshalJSON(data []byte) error {
-	var s string
-	err := json.Unmarshal(data, &s)
-	if err != nil {
-		return err
-	}
-
-	switch s {
-	case conventionType[ConventionalCommitConvention]:
-		*c = ConventionalCommitConvention
-	case conventionType[GitmojiConvention]:
-		*c = GitmojiConvention
-	default:
-		log.Fatal("unsupported convention", "type", s)
-	}
-
-	return nil
-}
-
-func (c ConventionType) MarshalJSON() ([]byte, error) {
-	return json.Marshal(conventionType[c])
-}
 
 type Config struct {
 	Convention ConventionType `json:"convention"`
